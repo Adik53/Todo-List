e80f4fd94e8cd739f0a54e4987c6a8c71d8b08ea
Adik53 <102859439+Adik53@users.noreply.github.com>
2023-07-02 23:26:55 +0300

    *
    *

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,83 +1,146 @@
-import './App.css'
-import {useState} from "react";
+import './App.css';
+import { useState } from "react";
 import Form from "../Form/Form";
 
-
 function App() {
-    const [todos,setTodos] = useState([])
-    const [allTodos,setAllTodos] = useState(0)
-    const [allComplete,setAllComplete] = useState(0)
+    const [todos, setTodos] = useState([]);
+    const [allTodos, setAllTodos] = useState(0);
+    const [allComplete, setAllComplete] = useState(0);
+    const [editTodo, setEditTodo] = useState(null);
 
-    const putTodo =(value) =>{
-        if (value){
-            setTodos([...todos,{id: Date.now(), text: value, done:false}])
-            setAllTodos(allTodos +1)
+    const putTodo = (value) => {
+        if (value) {
+            setTodos([...todos, { id: Date.now(), text: value, done: false }]);
+            setAllTodos(allTodos + 1);
+        } else {
+            alert("Please enter a valid todo.");
         }
-        else {
-            alert("bir şeyler yazmalısın")
-        }
-    }
-
-    const toggleTodo = (id) => {
-        setTodos(todos.map(todo => {
-            if (todo.id !== id) return todo;
-
-            const updatedTodo = {
-                ...todo,
-                done: !todo.done
-            };
-
-            if (updatedTodo.done) {
-                setAllComplete(allComplete + 1);
-            } else {
-                setAllComplete(allComplete - 1);
-            }
-
-            return updatedTodo;
-        }));
     };
 
-    const removeTodo = (id) =>{
-        setTodos(todos.filter(todo => todo.id !==id))
-        setAllTodos(allTodos -1)
-        setAllComplete(allComplete -1)
-    }
+    const toggleTodo = (id) => {
+        setTodos(
+            todos.map((todo) => {
+                if (todo.id !== id) return todo;
+
+                const updatedTodo = {
+                    ...todo,
+                    done: !todo.done
+                };
+
+                if (updatedTodo.done) {
+                    setAllComplete(allComplete + 1);
+                } else {
+                    setAllComplete(allComplete - 1);
+                }
+
+                return updatedTodo;
+            })
+        );
+    };
+
+    const removeTodo = (id) => {
+        setTodos(todos.filter((todo) => todo.id !== id));
+        setAllTodos(allTodos - 1);
+        if (editTodo && editTodo.id === id) {
+            setEditTodo(null);
+        }
+    };
+
+    const editTodoItem = (id) => {
+        const todoToEdit = todos.find((todo) => todo.id === id);
+        if (todoToEdit) {
+            setEditTodo({
+                id: todoToEdit.id,
+                text: todoToEdit.text
+            });
+        }
+    };
+
+    const updateTodo = (id, updatedText) => {
+        setTodos(
+            todos.map((todo) => {
+                if (todo.id !== id) return todo;
+
+                return {
+                    ...todo,
+                    text: updatedText
+                };
+            })
+        );
+        setEditTodo(null);
+    };
 
     const clearTodos = () => {
-      setTodos([]);
-      setAllTodos(0)
-            setAllComplete(0)
-    }
+        setTodos([]);
+        setAllTodos(0);
+        setAllComplete(0);
+        setEditTodo(null);
+    };
 
-  return(
-      <div className="wrapper">
-          <div className="container">
-              <h1 className="title">TodoList</h1>
-              <p>Adilet Kairzhanov 21703921</p>
-              <Form
-                  putTodo={putTodo}
-              />
-              <ul className= "todos">
-                  {
-                      todos.map(todo =>{
-                          return (
-                              <li className={todo.done ? "todo done" : "todo"} key={todo.id} onClick={() => toggleTodo(todo.id)}>
-                                  {todo.text}
-                                  <span className="delete" onClick={e => {e.stopPropagation(); removeTodo(todo.id); }}>
-                                      ❌
-                                  </span>
-                              </li>
-                          )
-                      })
-                  }
-                  <div className="info">
-                      <span> Tüm Todolist: {allTodos}</span>
-                      <span> Tamamlamak: {allComplete}</span>
-                  </div>
-                  <button className="clear" onClick={clearTodos}>Hepsini Sil</button>
-              </ul>
-          </div>
-      </div>
-  )
+    return (
+        <div className="wrapper">
+            <div className="container">
+                <h1 className="title">TodoList</h1>
+                <Form putTodo={putTodo} />
+                <ul className="todos">
+                    {todos.map((todo) => {
+                        if (editTodo && editTodo.id === todo.id) {
+                            return (
+                                <li className="todo" key={todo.id}>
+                                    <input
+                                        type="text"
+                                        value={editTodo.text}
+                                        onChange={(e) =>
+                                            setEditTodo({
+                                                ...editTodo,
+                                                text: e.target.value
+                                            })
+                                        }
+                                    />
+                                    <button
+                                        className="save"
+                                        onClick={() => updateTodo(editTodo.id, editTodo.text)}
+                                    >
+                                        Save
+                                    </button>
+                                </li>
+                            );
+                        } else {
+                            return (
+                                <li
+                                    className={todo.done ? "todo done" : "todo"}
+                                    key={todo.id}
+                                    onClick={() => toggleTodo(todo.id)}
+                                >
+                                    {todo.text}
+                                    <span
+                                        className="delete"
+                                        onClick={(e) => {
+                                            e.stopPropagation();
+                                            removeTodo(todo.id);
+                                        }}>
+                                        ❌
+                                    </span>
+                                    <span
+                                        className="edit"
+                                        onClick={() => editTodoItem(todo.id)}>
+                                        ✎
+                                    </span>
+                                </li>
+                            );
+                        }
+                    })}
+                </ul>
+                <div className="info">
+                    <span>All todos: {allTodos}</span>
+                    <span>Complete: {allComplete}</span>
+                </div>
+                <button className="clear" onClick={clearTodos}>
+                    Clear All
+                </button>
+            </div>
+        </div>
+    );
 }
+
 export default App;
--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,18 +1,128 @@
-import './Form.css'
+import './App.css';
 import {useState} from "react";
+import Form from "../Form/Form";
 
+function App() {
+    const [todos, setTodos] = useState([]);
+    const [allTodos, setAllTodos] = useState(0);
+    const [allComplete, setAllComplete] = useState(0);
+    const [editTodo, setEditTodo] = useState(null);
 
-const Form = (props) => {
-    const [value, setValue] = useState ("");
+    const putTodo = (value) => {
+        if (value) {
+            setTodos([...todos, {id: Date.now(), text: value, done: false}]);
+            setAllTodos(allTodos + 1);
+        } else {
+            alert("Bir şeyler yazmalısın");
+        }
+    };
+
+    const toggleTodo = (id) => {
+        setTodos(todos.map((todo) => {
+            if (todo.id !== id) return todo;
+
+            const updatedTodo = {
+                ...todo,
+                done: !todo.done
+            };
+
+            if (updatedTodo.done) {
+                setAllComplete(allComplete + 1);
+            } else {
+                setAllComplete(allComplete - 1);
+            }
+
+            return updatedTodo;
+        }));
+    };
+
+    const removeTodo = (id) => {
+        setTodos(todos.filter((todo) => todo.id !== id));
+        setAllTodos(allTodos - 1);
+        setAllComplete(allComplete - 1);
+    };
+
+    const editTodoItem = (id) => {
+        const todoToEdit = todos.find((todo) => todo.id === id);
+        setEditTodo(todoToEdit);
+    };
+
+    const updateTodo = (id, newText) => {
+        setTodos(todos.map((todo) => {
+            if (todo.id === id) {
+                return {
+                    ...todo,
+                    text: newText
+                };
+            }
+            return todo;
+        }));
+        setEditTodo(null);
+    };
+
+    const clearTodos = () => {
+        setTodos([]);
+        setAllTodos(0);
+        setAllComplete(0);
+    };
+
     return (
-        <form className="form"
-            onSubmit={e => {e.preventDefault();
-            props.putTodo(value);
-            setValue("");}
-        }>
-            <input type="text" placeholder="Buraya bir şeyler yaz" className="input" value={value} onChange={e => setValue(e.target.value)}/>
-        </form>
+        <div className="wrapper">
+            <div className="container">
+                <h1 className="title">TodoList</h1>
+                <p>Adilet Kairzhanov 21703921</p>
+                <Form putTodo={putTodo}/>
+                <ul className="todos">
+                    {todos.map((todo) => {
+                        if (editTodo && editTodo.id === todo.id) {
+                            return (
+                                <li className={todo.done ? "todo done" : "todo"} key={todo.id}>
+                                    <input
+                                        type="text"
+                                        value={editTodo.text}
+                                        onChange={(e) => updateTodo(todo.id, e.target.value)}
+                                    />
+                                    <button className="save">Save</button>
+                                    { }
+                                </li>
+                            );
+                        } else {
+                            return (
+                                <li
+                                    className={todo.done ? "todo done" : "todo"}
+                                    key={todo.id}
+                                    onClick={() => toggleTodo(todo.id)}
+                                >
+                                    {todo.text}
+                                    <span
+                                        className="delete"
+                                        onClick={(e) => {
+                                            e.stopPropagation();
+                                            removeTodo(todo.id);
+                                        }}>
+                                        ❌
+                                    </span>
+                                    <span
+                                        className="edit"
+                                        onClick={(e) => {
+                                            e.stopPropagation();
+                                            editTodoItem(todo.id);
+                                        }}>
+                                        ✏️
+                                    </span> {}
+                                </li>
+                            );
+                        }
+                    })}
+                </ul>
+                <div className="info">
+                    <span> Tüm Todolist: {allTodos}</span>
+                    <span> Tamamlamak: {allComplete}</span>
+                </div>
+                <button className="clear" onClick={clearTodos}>Hepsini Sil</button>
+            </div>
+        </div>
     );
-};
+}
 
-export default Form;
+export default App;
